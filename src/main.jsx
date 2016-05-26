4bbde3e9a2d3dbea45a8313a087d5f7577a62bb8
peter <peter.k.nets@gmail.com>
2016-05-26 11:48:43 +0300

    Attempt to use fetch

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'whatwg-fetch';
 
 import InputField from './components/input-field.jsx';
 import List from './components/list.jsx';
 
 
 class Main extends React.Component {
+
+  handleClick() {
+    fetch('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
   render() {
     return (
       <div>
         <h1>Wiki Viewer</h1>
         <InputField />
         <List />
+        <button onClick={this.handleClick}>Click Me!</button>
       </div>
     );
   }
