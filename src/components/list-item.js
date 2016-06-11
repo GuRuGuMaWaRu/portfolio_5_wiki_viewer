dcadecb4aa09a66650108c6b846209b798f53026
peter <peter.k.nets@gmail.com>
2016-06-11 13:21:05 +0300

    First variant of division amination

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
-const ListItem = (props) => {
-  return (
-    <a href={props.href} target="blank">
-      <div className="my-search-result">
-          <h5>{props.title}</h5>
-          <p>{props.text}</p>
+class ListItem extends React.Component {
+  // console.log(document.getElementsByClassName('my-list-item'));
+  // document.getElementsByClassName('my-list-item').className = 'animated';
+  componentDidMount() {
+    // document.getElementsByClassName('my-list-item').className = 'my-list-item animated';
+  }
+
+  render() {
+    return (
+      <div className="my-list-item">
+        <a href={this.props.href} target="blank">
+          <div className="my-search-result">
+              <h5>{this.props.title}</h5>
+              <p>{this.props.text}</p>
+          </div>
+        </a>
       </div>
-      <hr />
-    </a>
-  );
+    );
+  }
 }
 
 export default ListItem;
--- src/components/list.js
+++ src/components/list.js
@@ -1,37 +1,60 @@
 import React from 'react';
 import ListItem from './list-item.js';
 
-const List = (props) => {
-  let itemlist = [],
-      i = 0;
+class List extends React.Component {
+  componentDidUpdate() {
+    if (this.props.results.length > 0) {
+      // console.log(document.getElementsByClassName('my-list-item'));
+      $('.my-list-item').addClass('animated');
+      $('.my-list-item').animate({
+        marginTop: '15px'
+      }, 300);
 
-  if (props.results.length > 0) {
-    if (props.results[2][0].substr(-9) === 'can mean:') {
-      for (let i = 1, length = props.results[1].length; i < length; i++ ) {
-        itemlist.push(<ListItem key={i} title={props.results[1][i]} text={props.results[2][i]} href={props.results[3][i]} />);
-      }
-    } else {
-      for (let i = 0, length = props.results[1].length; i < length; i++ ) {
-        itemlist.push(<ListItem key={i} title={props.results[1][i]} text={props.results[2][i]} href={props.results[3][i]} />);
-      }
+      // let listItems = document.getElementsByClassName('my-list-item');
+      // console.log(listItems);
+      // listItems.map(function(item) {
+      //   console.log(item);
+      //   return item.className = 'animated';
+      // });
+      // console.log(listItems);
+      // for (let i = 0, length = listItems.length; i < length; i++) {
+      //   console.log(listItems[i]);
+        // console.log(listItems[i].className);
+        // listItems[i].className += 'bu';
+      //   listItems[i].addClass('bu');
+      //   console.log(listItems[i].className);
+
+        // listItems[i].className = 'animated';
+      // }
     }
-    return (
-      <div className="my-list">
-        Populated List
-        {itemlist}
-      </div>
-    );
-  } else {
-    return (
-      <div className="my-list">
-      Empty List
-      </div>
-    );
+  }
+
+  render() {
+    let itemlist = [],
+        i = 0;
+
+    if (this.props.results.length > 0) {
+      if (this.props.results[2][0].substr(-9) === 'can mean:') {
+        for (let i = 1, length = this.props.results[1].length; i < length; i++ ) {
+          itemlist.push(<ListItem key={i} title={this.props.results[1][i]} text={this.props.results[2][i]} href={this.props.results[3][i]} />);
+        }
+      } else {
+        for (let i = 0, length = this.props.results[1].length; i < length; i++ ) {
+          itemlist.push(<ListItem key={i} title={this.props.results[1][i]} text={this.props.results[2][i]} href={this.props.results[3][i]} />);
+        }
+      }
+      return (
+        <div>
+          {itemlist}
+        </div>
+      );
+    } else {
+      return (
+        <div className="my-list">
+        </div>
+      );
+    }
   }
 }
 
 export default List;
-
-{/*<div>
-  {itemlist}
-</div>*/}
