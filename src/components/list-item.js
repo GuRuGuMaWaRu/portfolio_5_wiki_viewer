a428f0154327bdc51f7e39fdcd5c5a6c3622b0fd
peter <peter.k.nets@gmail.com>
2016-06-15 12:43:32 +0300

    Solve an issue with list animation

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -4,7 +4,7 @@ class ListItem extends React.Component {
 
   render() {
     return (
-      <div className={this.props.animated ? "my-list-item cascaded" : "my-list-item"} style={{opacity: this.props.itemOpacity}}>
+      <div className={this.props.animated ? "my-list-item cascaded" : "my-list-item"}>
         <a href={this.props.href} target="blank">
           <div style={{opacity: this.props.textOpacity}} className="my-search-result">
               <h4>{this.props.title}</h4>
