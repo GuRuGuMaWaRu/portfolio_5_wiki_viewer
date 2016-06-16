f1e396ac02ad37adf4f1fa4a860c50ecc3b950ee
peter <peter.k.nets@gmail.com>
2016-06-16 10:25:05 +0300

    Use regex to remove invalid search results

--- src/components/list.js
+++ src/components/list.js
@@ -4,11 +4,13 @@ import ListItem from './list-item.js';
 const List = (props) => {
 
   let itemlist = [],
-        i = 0;
+      i = 0,
+      invalidEntry1 = /can mean/,
+      invalidEntry2 = /refer to/;
 
-  function build(iterator) {
+  function buildList(iterator) {
     for (let i = iterator, length = props.results[1].length; i < length; i++ ) {
-      if (props.results[2][i].substr(-9) === 'can mean:' || props.results[2][i].substr(-9) === 'refer to:') {
+      if (invalidEntry1.test(props.results[2][i]) || invalidEntry2.test(props.results[2][i])) {
         continue;
       } else {
         itemlist.push(<ListItem animated={props.animated} textOpacity={props.textOpacity}
@@ -18,7 +20,7 @@ const List = (props) => {
     }
   }
 
-  function buildEmpty() {
+  function buildEmptyList() {
     itemlist.push(<ListItem animated={false} textOpacity={1} key={0} title="" centerText={"center"} text="Please enter a valid search request" href="#" />);
     for (let i = 1, length = 5; i < length; i++ ) {
       itemlist.push(<ListItem animated={false} textOpacity={props.textOpacity} key={i} title="" text="" href="#" />);
@@ -27,9 +29,9 @@ const List = (props) => {
 
   if (props.results.length > 0) {
     if (props.results[1].length === 0) {
-      buildEmpty();
+      buildEmptyList();
     } else {
-      build(0);
+      buildList(0);
     }
     return (
       <div className="my-list">
@@ -37,7 +39,7 @@ const List = (props) => {
       </div>
     );
   } else {
-    buildEmpty();
+    buildEmptyList();
     return (
       <div className="my-list">
         {itemlist}
